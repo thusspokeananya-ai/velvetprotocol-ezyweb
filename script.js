5a1485d090c5b9f758b75d6e8625e99588f9d7fe
sirisr700-hub <sirisr700@gmail.com>
2026-04-28 13:04:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+let size = 16;
 
+function increaseFont() {
+    size += 2;
+    document.getElementById("text").style.fontSize = size + "px";
+}
+
+function decreaseFont() {
+    size -= 2;
+    document.getElementById("text").style.fontSize = size + "px";
+}
+
+function toggleDark() {
+    document.body.classList.toggle("dark");
+}
+
+function readText() {
+    let text = document.getElementById("text").innerText;
+
+    let speech = new SpeechSynthesisUtterance(text);
+    speechSynthesis.speak(speech);
+}
+
+function simplifyText() {
+    document.getElementById("simpleText").innerText =
+    "Government websites are often difficult. EasyWeb makes them simpler to understand.";
+}
